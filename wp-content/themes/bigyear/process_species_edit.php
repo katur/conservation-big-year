4c514f897f5e69cc00fca14616287dfefa42147f
Katherine Erickson <katherine.erickson@gmail.com>
2013-06-27 18:46:33 -0400

    add page to edit species information

---
+++ wp-content/themes/bigyear/process_species_edit.php
@@ -0,0 +1,27 @@
+<?php	
+	include("katherine_connect.php");
+	/*
+	Template Name: Process Species Edit
+	Copyright (c) 2013 Katherine Erickson
+	*/
+	
+	// get the common name from the GET variable (visible in the url). 
+	$url_common_name = mysql_real_escape_string($_GET['common_name']);
+
+	// grab variables from POST variable
+	$flickr_code = $_POST['flickr_code'];
+	$cornell_map = $_POST['cornell_map'];
+	$ebird_map = $_POST['ebird_map'];
+	$essay = $_POST['essay'];
+	
+	$query = "UPDATE species_list
+		SET flickr_code = '$flickr_code', essay = '$essay',
+		ebird_map = '$ebird_map', cornell_map = '$cornell_map'
+		WHERE url_common_name = '$url_common_name'
+	";
+
+	$result = mysql_query($query) or die(mysql_error());
+	
+	// set the header to redirect to VIEW page
+	wp_redirect("/species/?common_name=$url_common_name");
+?>
--- wp-content/themes/bigyear/species_edit.php
+++ wp-content/themes/bigyear/species_edit.php
@@ -4,82 +4,54 @@
 	Template Name: Species Edit
 	Copyright (c) 2013 Katherine Erickson
 	*/
-?>
+	
+	// get the common name from the GET variable (visible in the url). 
+	$url_common_name = mysql_real_escape_string($_GET['common_name']);
+		
+	$query = "SELECT
+		common_name, flickr_code, essay, ebird_map, cornell_map
+		FROM species_list
+		WHERE url_common_name = '$url_common_name'
+	";
 
-<div id="site-content full-width">
-	Edit the maps, essay, or flickr code for a particular species.
+	// run query
+	$result = mysql_query($query) or die(mysql_error());
+	if (mysql_num_rows($result) == 0) {
+		echo "$url_common_name is not a bird.";
+	} else if (mysql_num_rows($result) > 1) {
+		echo "Too many birds match that url common name.";
+	} else { // if one result
+		// Define variables
+		$row = mysql_fetch_assoc($result);
+		$common_name = $row["common_name"];
+		$flickr_code = $row["flickr_code"];
+		$essay = $row["essay"];
+		$cornell_map = $row["cornell_map"];
+		$ebird_map = $row["ebird_map"];
 
-	<?php
-		// get the common name from the GET variable (visible in the url). 
-		// the php-format url is available via htaccess file.
-		$url_common_name = mysql_real_escape_string($_GET['url_common_name']);
-
-		// If there is data from a POST
-		if ($_POST) {		
-			// grab the variables from the POST variable
-			$flickr_code = mysql_real_escape_string($_POST['what_issues']);
-			$cornell_map = mysql_real_escape_string($_POST['cornell_map']);
-			$ebird_map = mysql_real_escape_string($_POST['ebird_map']);
-			$essay = mysql_real_escape_string($_POST['why_concern']);
-
-			$query = "UPDATE species_list
-				SET flickr_code = '$flickr_code', essay = '$essay',
-				ebird_map = '$ebird_map', cornell_map = '$cornell_map'
-				WHERE url_common_name = '$url_common_name'
-			";
-
-			$result = mysql_query($query) or die(mysql_error());
-
-			// php function to set the header to redirect to VIEW page
-			header("Location: /species/$url_common_name");
-
-		// if there is not data from a post	
-		} else {
-			// define MySQL query to access fields for current species
-			$query = "SELECT
-				flickr_code, essay, ebird_map, cornell_map
-				FROM species_list
-				WHERE url_common_name = '$url_common_name'
-			";
-
-			// run query
-			$result = mysql_query($query) or die(mysql_error());
-
-			// if no result
-			if (mysql_num_rows($result) == 0) {
-				echo "That is not a bird.";
+		echo "
+			<div class='site-content full-width edit-species'>
+				<h2 class='species-subtitle'>
+					Edit information for <b>$common_name</b>.
+				</h2>
 			
-			} else if (mysql_num_rows($result) > 1) {
-				echo "Too many birds match that url common name.";
-
-			} else { // if one result
-				// Define variables
-				$row = mysql_fetch_assoc($result))
-				$common_name = $row["common_name"];
-				$flickr_code = $row["flickr_code"];
-				$essay = $row["essay"];
-				$cornell_map = $row["cornell_map"];
-				$ebird_map = $row["ebird_map"];
-
-				?>		
-				
-				<h1><?php echo $common_name; ?></h1>
-				<form method='post' action='./<?php echo $url_common_name; ?>'>
-					<h3>Main Photo</h3>
+				<form method='post' action='/process-species-edit/?common_name=$url_common_name'>
+					<h3>Flickr Code for main photo (\"medium\", 500px max dimension)</h3>
+					<textarea name='flickr_code' rows='3' cols='110'>$flickr_code</textarea>
 										
+					<h3>Essay</h3>
+					<textarea name='essay' rows='10' cols='110'>$essay</textarea>
+					
 					<h3>Cornell Range Map</h3>
-					<input name='cornell_map' type='text' size='150' value='<?php echo $cornell_map; ?>'>
+					<input name='cornell_map' type='text' size='150' value='$cornell_map'>
 						
 					<h3>Ebird Dynamic Map</h3>
-					<input name='ebird_map' type='text' size='150' value='<?php echo $ebird_map; ?>'>
-					
-					<h3>Essay</h3>
-					<textarea name='essay' rows='100' cols='100'><?php echo $essay; ?></textarea>
+					<input name='ebird_map' type='text' size='150' value='$ebird_map'>
 				
 					<br><br>
 					<input type='submit' value='Submit to Database'></input>
 				</form>
-	<?php	}
+			</div>
+		";
 	}
-	?>	
-</div>
+?>
