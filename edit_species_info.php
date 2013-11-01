42571c4f35fc0bc049277311b96d11242d1ec110
katur <ke14@nyu.edu>
2013-11-01 17:10:44 -0400

    add CMS for refuge stuff

--- edit_species_info.php
+++ edit_species_info.php
@@ -1,14 +1,15 @@
-<?php get_header(); 
+<?php get_header();
 	include("katherine_connect.php");
 	/*
 	Template Name: Edit Species Info
 	Copyright (c) 2013 Katherine Erickson
 	*/
-	
-	// get the common name from the GET variable (visible in the url). 
+
+	// get the common name from the GET variable (visible in the url).
 	$url_common_name = mysql_real_escape_string($_GET['common_name']);
-		
+
 	$query = "SELECT
+		seen_in_refuge, seen_only_in_refuge,
 		common_name, flickr_code, essay, ebird_map, cornell_map
 		FROM species_list
 		WHERE url_common_name = '$url_common_name'
@@ -23,6 +24,8 @@
 	} else { // if one result
 		// Define variables
 		$row = mysql_fetch_assoc($result);
+		$seen_in_refuge = $row["seen_in_refuge"];
+		$seen_only_in_refuge = $row["seen_only_in_refuge"];
 		$common_name = $row["common_name"];
 		$flickr_code = $row["flickr_code"];
 		$essay = $row["essay"];
@@ -34,20 +37,26 @@
 				<h2 class='species-subtitle'>
 					Edit information for <b>$common_name</b>
 				</h2>
-			
+
 				<form method='post' action='/process-species-info/?common_name=$url_common_name'>
+					<h3>Seen within National Wildlife Refuge System</h3>
+					<input name='seen_in_refuge' type='text' size='1' value='$seen_in_refuge'>
+
+					<h3>Seen only within National Wildlife Refuge System</h3>
+					<input name='seen_only_in_refuge' type='text' size='1' value='$seen_only_in_refuge'>
+
 					<h3>Flickr Code for main photo (\"medium\")</h3>
 					<textarea name='flickr_code' rows='5' cols='110'>$flickr_code</textarea>
-										
+
 					<h3>Essay</h3>
 					<textarea name='essay' rows='12' cols='110'>$essay</textarea>
-					
+
 					<h3>Cornell Range Map</h3>
 					<input name='cornell_map' type='text' size='144' value='$cornell_map'>
-						
+
 					<h3>Ebird Dynamic Map</h3>
 					<input name='ebird_map' type='text' size='144' value='$ebird_map'>
-				
+
 					<br><br>
 					<input type='submit' value='Submit to Database'></input>
 				</form>
--- process_species_info.php
+++ process_species_info.php
@@ -1,14 +1,16 @@
-<?php	
+<?php
 	include("katherine_connect.php");
 	/*
 	Template Name: Process Species Info
 	Copyright (c) 2013 Katherine Erickson
 	*/
-	
-	// get the common name from the GET variable (visible in the url). 
+
+	// get the common name from the GET variable (visible in the url).
 	$url_common_name = mysql_real_escape_string($_GET['common_name']);
 
 	// grab variables from POST variable
+	$seen_in_refuge = $_POST['seen_in_refuge'];
+	$seen_only_in_refuge = $_POST['seen_only_in_refuge'];
 	$flickr_code = $_POST['flickr_code'];
 	$cornell_map = $_POST['cornell_map'];
 	$ebird_map = $_POST['ebird_map'];
@@ -18,23 +20,25 @@
 		$width = preg_replace("/.+width=\"(.+?)\".+/", "$1", stripcslashes($flickr_code));
 		$height = preg_replace("/.+height=\"(.+?)\".+/", "$1", stripcslashes($flickr_code));
 		$query = "UPDATE species_list
-			SET flickr_code = '$flickr_code', flickr_src = '$src', 
-			flickr_width = '$width', flickr_height = '$height', essay = '$essay',
-			ebird_map = '$ebird_map', cornell_map = '$cornell_map'
-			WHERE url_common_name = '$url_common_name'
+			SET flickr_code='$flickr_code', flickr_src='$src',
+			flickr_width='$width', flickr_height='$height', essay='$essay',
+			ebird_map='$ebird_map', cornell_map='$cornell_map',
+			seen_in_refuge='$seen_in_refuge', seen_only_in_refuge='$seen_only_in_refuge'
+			WHERE url_common_name='$url_common_name'
 		";
 	} else {
 		$query = "UPDATE species_list
-			SET flickr_code = NULL, flickr_src = NULL, 
-			flickr_width = NULL, flickr_height = NULL, essay = '$essay',
-			ebird_map = '$ebird_map', cornell_map = '$cornell_map'
-			WHERE url_common_name = '$url_common_name'
+			SET flickr_code=NULL, flickr_src=NULL,
+			flickr_width=NULL, flickr_height=NULL, essay='$essay',
+			ebird_map='$ebird_map', cornell_map='$cornell_map',
+			seen_in_refuge='$seen_in_refuge', seen_only_in_refuge='$seen_only_in_refuge'
+			WHERE url_common_name='$url_common_name'
 		";
 	}
 
 
 	$result = mysql_query($query) or die(mysql_error());
-	
+
 	// set the header to redirect to VIEW page
 	wp_redirect("/species/?common_name=$url_common_name");
 ?>
